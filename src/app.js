075bd148f653cd0c800d8275ff84c348fc164b80
YASH AMANAJI <yash@Macbook-Air.local>
2022-06-23 12:20:26 +0530

    geocode changes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
-const geocode = require('./utils/geoCode')
+const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
